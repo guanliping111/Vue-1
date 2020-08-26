e257d6d194d3a7242970072baa9a7dea61c0098e
guanliping <2289029828@qq.com>
2020-08-26 09:22:58 +0800

    vue源码分析

---
+++ element-code/README.md
@@ -0,0 +1,24 @@
+1. cnpm install -g @vue/cli    安装vue
+2. vue create element-code     创建项目
+3. yarn add element-ui -S      引入element-ui
+
+- vue-cli 4. vue3.0 变化太大 拥抱了react
+- vue create project-name
+    vue 2.0 vue init webpack project-name 传统 现在流行
+    vue 3.0 vue create project-name
+    - ts
+    - PWA 渐进式网页应用
+    - Router Vuex 全家桶
+    - CSS Pre-processors stylus vw autoprefixer
+        编译 sass/stylue 插件 autoprefixer 前缀
+        px-vw-   cssnext
+    - liner 代码风格确保文件 ; '' "" tab
+        eslint 保证大家代码风格都一样
+        不用的变量不要定义，要删除干净
+        对齐代码
+        '' ""
+        js的结尾不加;
+
+    - unit Testing  E2E 测试
+
+## element源码解析
---
+++ element-code/src/components/ElButton.vue
@@ -0,0 +1,47 @@
+<template>
+    <button
+        @click="handleClick"
+        class="el-button"
+        :class="[
+        type?'el-button--'+type:'',
+        {
+            'is-round': round,
+            'is-circle': circle
+        }
+        ]"
+    >
+        <!-- {{type}} - {{tit}} -->
+       <slot></slot>
+    </button>
+</template>
+<script>
+// props 检查 结构
+// vue 怎么做
+export default {
+    data() {
+        return {
+            tit: 'hello'
+        }
+    },
+   props: {
+       type: {
+           type: String,
+           default: 'default'
+       },
+       round: Boolean,
+       circle: Boolean,
+   },
+   created() {
+       console.log(this.type, this.tit)
+   },
+   methods: {
+       handleClick(evt) {
+           this.$emit('click',evt);
+       }
+   }
+}
+</script>
+
+<style>
+
+</style>
